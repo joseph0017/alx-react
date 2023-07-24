5804055702d176e82952792bbc0d9f274289d232
joseph0017 <josephogiku96@gmail.com>
2023-07-24 10:21:13 +0100

    webpack

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+const dollar = $
+
+dollar('body').append('<p>Holberton Dashboard</p>')
+dollar('body').append('<p>Dashboard data for the students</p>')
+dollar('body').append('<p>Copyright - Holberton School</p>')
